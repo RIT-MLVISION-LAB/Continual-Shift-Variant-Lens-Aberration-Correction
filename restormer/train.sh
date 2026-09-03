496838ce24ffd1f4c62cc77b49f84e7749404406
Alif Ashrafee <aa5264@rit.edu>
2026-09-03 12:21:39 -0400

    Standardized training configs created for Restormer and PromptIR backbones

--- restormer/train.sh
+++ restormer/train.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Multi-GPU training script for Restormer
+# Single-GPU training script
 # Usage: ./train.sh <config.yml>
 
 CONFIG=$1
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PYTHONPATH="${SCRIPT_DIR}:${PYTHONPATH}"
 
-torchrun --nproc_per_node=4 --master_port=4321 basicsr/train.py -opt $CONFIG --launcher pytorch
+python basicsr/train.py -opt $CONFIG
